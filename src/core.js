38d32af28154dbaf4163168afef88b34335ce9b8
Joe McCourt <joe@aviary.com>
2013-08-18 16:43:24 -0400

    init commit, copy of shortestpath game

---
+++ src/core.js
@@ -0,0 +1,222 @@
+JSPG.levels = {
+	level1 : "{\"w\":10,\"h\":10,\"start\":[1,5],\"finish\":[8,5],\"tiles\":[{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":40,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":40,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":40,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":40,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"start\"},{\"value\":20,\"type\":\"goal\"},{\"value\":20,\"type\":\"goal\"},{\"value\":100,\"type\":\"goal\"},{\"value\":20,\"type\":\"goal\"},{\"value\":20,\"type\":\"goal\"},{\"value\":20,\"type\":\"goal\"},{\"value\":20,\"type\":\"finish\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\",\"visited\":true},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":40,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\",\"visited\":true},{\"value\":20,\"type\":\"normal\",\"visited\":true},{\"value\":20,\"type\":\"normal\",\"visited\":true},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\",\"visited\":true},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":40,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\",\"visited\":true},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\",\"visited\":true},{\"value\":20,\"type\":\"normal\",\"visited\":true},{\"value\":20,\"type\":\"normal\",\"visited\":true},{\"value\":40,\"type\":\"normal\",\"visited\":true},{\"value\":20,\"type\":\"normal\",\"visited\":true},{\"value\":20,\"type\":\"normal\",\"visited\":true},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"}],\"shortestPath\":[[1,5],[2,5],[3,5],[3,6],[4,6],[5,6],[6,6],[7,6],[8,6],[8,5]]}",
+	level2 : "{\"w\":10,\"h\":10,\"start\":[1,5],\"finish\":[8,5],\"tiles\":[{\"value\":130,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":85,\"type\":\"normal\"},{\"value\":5,\"type\":\"normal\"},{\"value\":0,\"type\":\"normal\"},{\"value\":55,\"type\":\"normal\"},{\"value\":15,\"type\":\"normal\"},{\"value\":45,\"type\":\"normal\"},{\"value\":5,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":40,\"type\":\"normal\"},{\"value\":40,\"type\":\"normal\"},{\"value\":95,\"type\":\"normal\"},{\"value\":90,\"type\":\"normal\"},{\"value\":45,\"type\":\"normal\"},{\"value\":45,\"type\":\"normal\"},{\"value\":95,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":65,\"type\":\"normal\"},{\"value\":60,\"type\":\"normal\"},{\"value\":50,\"type\":\"normal\"},{\"value\":45,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":35,\"type\":\"normal\"},{\"value\":60,\"type\":\"normal\"},{\"value\":5,\"type\":\"normal\"},{\"value\":70,\"type\":\"normal\"},{\"value\":80,\"type\":\"normal\"},{\"value\":90,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":5,\"type\":\"normal\"},{\"value\":65,\"type\":\"normal\"},{\"value\":85,\"type\":\"normal\"},{\"value\":75,\"type\":\"normal\"},{\"value\":60,\"type\":\"normal\"},{\"value\":35,\"type\":\"normal\"},{\"value\":80,\"type\":\"normal\"},{\"value\":25,\"type\":\"normal\"},{\"value\":0,\"type\":\"normal\"},{\"value\":65,\"type\":\"normal\"},{\"value\":25,\"type\":\"normal\"},{\"value\":40,\"type\":\"normal\"},{\"value\":0,\"type\":\"normal\"},{\"value\":90,\"type\":\"normal\"},{\"value\":5,\"type\":\"normal\"},{\"value\":5,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":30,\"type\":\"normal\"},{\"value\":35,\"type\":\"normal\"},{\"value\":90,\"type\":\"normal\"},{\"value\":5,\"type\":\"normal\"},{\"value\":0,\"type\":\"start\"},{\"value\":90,\"type\":\"normal\"},{\"value\":75,\"type\":\"normal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":60,\"type\":\"normal\"},{\"value\":35,\"type\":\"normal\"},{\"value\":45,\"type\":\"normal\"},{\"value\":0,\"type\":\"finish\"},{\"value\":50,\"type\":\"normal\"},{\"value\":95,\"type\":\"normal\"},{\"value\":80,\"type\":\"goal\"},{\"value\":30,\"type\":\"normal\"},{\"value\":55,\"type\":\"normal\"},{\"value\":30,\"type\":\"normal\"},{\"value\":45,\"type\":\"normal\"},{\"value\":0,\"type\":\"goal\"},{\"value\":55,\"type\":\"goal\"},{\"value\":35,\"type\":\"goal\"},{\"value\":20,\"type\":\"normal\"},{\"value\":5,\"type\":\"normal\"},{\"value\":85,\"type\":\"goal\"},{\"value\":85,\"type\":\"normal\"},{\"value\":35,\"type\":\"normal\"},{\"value\":45,\"type\":\"normal\"},{\"value\":50,\"type\":\"normal\"},{\"value\":10,\"type\":\"goal\"},{\"value\":65,\"type\":\"normal\"},{\"value\":40,\"type\":\"normal\"},{\"value\":25,\"type\":\"normal\"},{\"value\":95,\"type\":\"normal\"},{\"value\":75,\"type\":\"goal\"},{\"value\":80,\"type\":\"goal\"},{\"value\":60,\"type\":\"goal\"},{\"value\":75,\"type\":\"goal\"},{\"value\":40,\"type\":\"goal\"},{\"value\":10,\"type\":\"goal\"},{\"value\":85,\"type\":\"normal\"},{\"value\":75,\"type\":\"normal\"},{\"value\":5,\"type\":\"normal\"},{\"value\":15,\"type\":\"normal\"},{\"value\":30,\"type\":\"normal\"},{\"value\":0,\"type\":\"normal\"},{\"value\":80,\"type\":\"normal\"},{\"value\":55,\"type\":\"normal\"},{\"value\":35,\"type\":\"normal\"},{\"value\":50,\"type\":\"normal\"},{\"value\":75,\"type\":\"normal\"},{\"value\":85,\"type\":\"normal\"},{\"value\":70,\"type\":\"normal\"}],\"shortestPath\":[[1,5],[1,4],[2,4],[3,4],[4,4],[5,4],[6,4],[7,4],[8,4],[8,5]]}"
+};
+
+JSPG.buildGoalPath = function(map){
+	var path = [];
+
+	console.log(map);
+	var w = map.w;
+	var h = map.h;
+
+	var start = map.start;
+
+	var index = start[0] + w*start[1];
+	var keepSearching = true;
+	var i = 0;
+	var x,y;
+
+	while(keepSearching && i < w*h){
+		x = index%w;
+		y = index/w|0;
+		path[i] = [x,y];
+
+		//Search neighbors
+		var k; //left,up,right,down
+		var neighborGoal = false;
+		for(k = 0; k < 4; k++){
+			if(k == 0){
+				if(x == 0){continue;}
+				index = x - 1 + y*w;
+			}else if(k == 1){
+				if(y == 0){continue;}
+				index = x + (y-1)*w;
+			}else if(k == 2){
+				if(x == w-1){continue;}
+				index = x + 1 + y * w;
+			}else if(k == 3){
+				if(y == h-1){continue;}
+				index = x + (y+1)*w;
+			}
+
+			if(map.tiles[index].type === "goal" && !map.tiles[index].visited){
+				map.tiles[index].visited = true;
+				neighborGoal = true;
+				break;
+			}else if(map.tiles[index].type === "finish"){
+				neighborGoal = true;
+				break;
+			}
+		}
+		if(!neighborGoal){
+			keepSearching = false;
+		}
+		i++;
+	}
+
+	return path;
+};
+
+JSPG.createMap = function(){
+	var map = {};
+
+	var w = 10;
+	var h = 10;
+
+	map.w = w;
+	map.h = h;
+
+	map.start = [0+1,h/2|0];
+	map.finish = [w-2,h/2|0];
+
+	map.tiles = [];
+
+	//Random map for now
+	var x,y;
+	var index;
+	var value,type;
+	for(y = 0; y < h; y++){
+		for(x = 0; x < w; x++){
+			index = x+w*y;
+			map.tiles[index] = {};
+
+			value = 5*Math.floor(20*Math.random());  //intervals of 5
+			type = "normal";
+
+			if(x == map.start[0] && y == map.start[1]){
+				value = 0;
+				type = "start";
+			}
+			
+			if(x == map.finish[0] && y == map.finish[1]){
+				value = 0;
+				type = "finish";
+			}
+
+			JSPG.setTileValue(map,x,y,value);
+			JSPG.setTileType(map,x,y,type);
+		}
+	}
+
+	map.shortestPath = JSPG.findShortestPath(map);
+	return map;
+};
+
+JSPG.setTileValue = function(map,x,y,value){
+	map.tiles[x+y*map.w].value = value;	
+};
+
+JSPG.setTileType = function(map,x,y,type){
+	map.tiles[x+y*map.w].type = type;	
+};
+
+// A rather basic implementation of Dijkstra's algorithm
+// A* seach would probably be faster, but this is fast enough
+// for the sized maps I plan to use
+JSPG.findShortestPath = function(map){
+	if(typeof map !== "object"){map = JSPG.map;}
+
+	var nodes = map.tiles;
+	var paths = [];
+
+	var w = map.w;
+	var h = map.h;
+	var index;
+	
+	// Init paths
+	var i;
+	for(i = 0; i < map.tiles.length; i++){
+		paths[i] = {previousNode:-1,pathValue:0,value:nodes[i].value,updated:false,reached:false}
+	}
+
+	// Start
+	index = map.start[0] + w*map.start[1];
+	paths[index] = {previousNode:-1,pathValue:0,value:0,updated:true,reached:true};
+
+	var finishedSearch = false;
+	var path;
+	var x,y;
+	while(!finishedSearch){
+		finishedSearch = true;
+
+		for(i = 0; i < nodes.length; i++){
+			path = paths[i];
+			if(path.reached){
+				if(path.updated){
+
+					path.updated = false;
+
+					x = i % w;
+					y = i / w | 0;
+
+					//Expand to neighbors
+					var k; //left,up,right,down
+					for(k = 0; k < 4; k++){
+						if(k == 0){
+							if(x == 0){continue;}
+							index = x - 1 + y*w;
+						}else if(k == 1){
+							if(y == 0){continue;}
+							index = x + (y-1)*w;
+						}else if(k == 2){
+							if(x == w-1){continue;}
+							index = x + 1 + y * w;
+						}else if(k == 3){
+							if(y == h-1){continue;}
+							index = x + (y+1)*w;
+						}
+
+						//If doesn't exist or shorter, update path
+						if(!paths[index].reached || path.pathValue+paths[index].value < paths[index].pathValue){
+							paths[index].previousNode = i;
+							paths[index].updated = true;
+							paths[index].reached = true;
+							paths[index].pathValue = path.pathValue+paths[index].value;
+
+							finishedSearch = false;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	//Build final path array
+	//Iterating backwards
+	var path = [];
+	var index = map.finish;
+	var startIndex = map.start[0]+w*map.start[1];
+	var finishIndex = map.finish[0]+w*map.finish[1];
+	var lastNodeIndex = finishIndex;
+	
+	var i = 0;
+	while(lastNodeIndex != startIndex && lastNodeIndex != -1){
+
+		path[i] = [lastNodeIndex%w,lastNodeIndex/w|0];
+
+		lastNodeIndex = paths[lastNodeIndex].previousNode;
+
+		i++;
+		if( i > 10000){break;} //safety
+	}
+
+	path[i] = [startIndex%w,startIndex/w|0];
+
+	return path.reverse();
+};
+
+JSPG.pathEqualsPath = function(path1,path2){
+
+	if(typeof path1 !== "object" || typeof path2 !== "object"){return false;}
+	if(path1.length !== path2.length){return false;}
+	var i;
+
+	for(i = 0; i < path1.length; i++){
+		if(path1[i][0] !== path2[i][0] || path1[i][1] !== path2[i][1]){
+			return false;
+		}
+	}
+
+	return true;
+};
